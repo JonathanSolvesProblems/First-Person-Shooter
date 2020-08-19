33db31fa0a6ea2810c5aa0add57efc007400af37
JonathanSolvesProblems <46333281+JonathanSolvesProblems@users.noreply.github.com>
2020-08-18 22:25:24 -0400

    added readme

---
+++ FirstPersonMaze/FirstPersonMaze/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+#include <Windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+int main()
+{
+	wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	while (1)
+	{
+
+	}
+
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+
+	return 0;
+}
